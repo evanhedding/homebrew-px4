8553bf2b9d5e77b1bc81419fa543455f8a62131d
evanhedding <51241782+evanhedding@users.noreply.github.com>
2023-08-18 16:19:10 -0400

    Update px4-sim-gazebo.rb

--- Formula/px4-sim-gazebo.rb
+++ Formula/px4-sim-gazebo.rb
@@ -27,7 +27,7 @@ class Px4SimGazebo < Formula
   depends_on "gst-plugins-ugly"
   depends_on "gstreamer"
   depends_on "opencv"
-  depends_on "osrf/simulation/gazebo11"
+  depends_on "evanhedding/simulation/gazebo11"
   depends_on "protobuf"
   depends_on "px4-dev"
 
